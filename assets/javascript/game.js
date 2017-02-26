f4b4c97bb87d6408afacdbb4ed7f0fb24c527245
Christina Rajaram <Christina Rajaram>
2017-02-26 13:17:46 -0500

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,16 @@
-//globle variables
+
 var wins = 0;
 var losses = 0;
-//players currunt score
+
 var addition = 0;
-//selects a random number from 19 t0 120
+
 var total = Math.floor((Math.random()* 100) + 19);
-//assigns random value to crystals from 1-12
-var red = Math.floor((Math.random()* 12) + 1);
+
+var purple = Math.floor((Math.random()* 12) + 1);
 var blue = Math.floor((Math.random()* 12) + 1);
 var yellow = Math.floor((Math.random()* 12) + 1);
 var green = Math.floor((Math.random()* 12) + 1);
-var audio = new Audio('https://files.slack.com/files-pri/T0J8XK2Q3-F0NHG3FDZ/download/raven.mp3');
 
-//updates addition in html
 var updateAddition = function (){
 	$('.addition').empty();
 	$('.addition').append(addition);
@@ -21,7 +19,7 @@ var updateAddition = function (){
 	$('#losses').empty();
 	$('#losses').append(losses);
 }
-//restart function
+
 var restart = function(){
 	addition = 0;
     total = Math.floor((Math.random()* 100) + 19);
@@ -29,23 +27,21 @@ var restart = function(){
 	$('.total').empty();
 	$('.total').append(total);
 
-	red = Math.floor((Math.random()* 12) + 1);
+	purple = Math.floor((Math.random()* 12) + 1);
     blue = Math.floor((Math.random()* 12) + 1);
     yellow = Math.floor((Math.random()* 12) + 1);
     green = Math.floor((Math.random()* 12) + 1);
-    //alert('restart');
+    
     updateAddition();
 }
 var logic = function (){
 	if (addition == total) {
 		wins = wins + 1;
 		
-		audio.play();
 		restart();
 	}else if(addition > total){
 		losses = losses + 1;
 		
-		audio.play();
 		restart();
 	}else{
 		updateAddition();
@@ -54,11 +50,11 @@ var logic = function (){
 $('.total').append(total);
 
 $('.addition').append(addition);
-//function
+
 
 $(document).ready(function(){
-	$('#red').click(function(){
-		addition = addition + red;
+	$('#purple').click(function(){
+		addition = addition + purple;
 		logic();
 	})
 	$('#blue').click(function(){
